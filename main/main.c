dae21e07276911e3dfc5269755d60515b99fc328
cauepda <cauepda@al.insper.edu.br>
2025-06-02 20:57:05 -0300

    update

--- main/main.c
+++ main/main.c
@@ -1,40 +1,25 @@
 /*
- * LED blink with FreeRTOS
+ * Copyright (c) 2022 Mr. Green's Workshop https://www.MrGreensWorkshop.com
+ *
+ * SPDX-License-Identifier: Apache-2.0
  */
-#include <FreeRTOS.h>
-#include <task.h>
-#include <semphr.h>
-#include <queue.h>
 
-
+#include "pico/cyw43_arch.h"
 #include "pico/stdlib.h"
-#include <stdio.h>
+#include "btstack_run_loop.h"
 
-void task_1(void *p) {
-    while (1) {
-	printf("hello from task 1 \n");
-	vTaskDelay(pdMS_TO_TICKS(500));
-    }
-}
-
-
-void task_2(void *p) {
-    char cnt = 15;
-    while (1) {
-
-	printf("hello from task 2 \n");
-	vTaskDelay(pdMS_TO_TICKS(1000));
-    }
-}
+int btstack_main(int argc, const char * argv[]);
 
 int main() {
     stdio_init_all();
 
-    xTaskCreate(task_1, "Task 1", 4095, NULL, 1, NULL);
-    xTaskCreate(task_2, "Task 2", 4095, NULL, 1, NULL);
-    
-    vTaskStartScheduler();
+    // initialize CYW43 driver
+    if (cyw43_arch_init()) {
+        printf("cyw43_arch_init() failed.\n");
+        return -1;
+    }
 
-    while (true)
-        ;
+    // run the app
+    btstack_main(0, NULL);
+    btstack_run_loop_execute();
 }
